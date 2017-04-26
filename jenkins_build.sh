9d9bda53a0cee6048e131d37b553120124b0fb48
hernad <hernad@bring.out.ba>
2017-04-26 16:48:20 +0200

    jenkins_build.sh

---
+++ jenkins_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if [ -f packer.zip ] ; then
+  curl -L https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_linux_amd64.zip?_ga=1.155119281.822261056.1493217676 > packer.zip
+  unzip packer.zip
+fi
+
+packer/packer build -only=virtualbox-iso arch-template.json
+
+
