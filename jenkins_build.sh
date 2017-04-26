df6b913286e2fd194e200d8b8bbd7fd1cd97ef70
hernad <hernad@bring.out.ba>
2017-04-26 16:53:57 +0200

    jenkins-vbox-arch

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ -f packer.zip ] ; then
+if [ ! -f packer.zip ] ; then
   curl -L https://releases.hashicorp.com/packer/1.0.0/packer_1.0.0_linux_amd64.zip?_ga=1.155119281.822261056.1493217676 > packer.zip
   unzip packer.zip
 fi
